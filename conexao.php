80f35a8492a654232b78b38c0d53da60071593ff
marcelafrutuoso <88401869+marcelafrutuoso@users.noreply.github.com>
2021-12-03 13:27:54 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,27 +1,26 @@
 <?php
-
-class conexao{
+class conexao {
   private $servidor;
   private $banco;
   private $usuario;
   private $senha;
-  private  static  $pdo ;
-  
-  public function __construct () {
-    $this ->servidor = 'localhost';
-    $this ->banco = 'av2';
-    $this ->usuario = 'root';
-    $this ->senha = '';
-  }
-  public function conectar() {
-    try {
-      if ( is_null ( self :: $pdo )) {
-        self :: $pdo = new  PDO( "mysql: host =" . $this->servidor . "; dbname =" . $this->banco , $this->usuario , $this->senha );
-      }
-      return self :: $pdo;
-    } catch ( PDOException $ex ) {
-      echo $ex ->getMessage();
-      }
-    }
-  }
+ private static $pdo;
+ 
+ public  function  __construct () {
+   $this ->servidor = 'localhost' ;
+   $this ->banco = 'av2' ;
+   $this ->usuario = 'root' ;
+   $this ->senha = '' ;
+ }
+  public function conectar () {
+   try {
+     if ( is_null ( self :: $pdo )) {
+       self :: $pdo = new   PDO ( "mysql: host =" . $this->servidor . "; dbname =" . $this->banco , $this->usuario , $this->senha );
+     }
+     return  self :: $pdo ;
+   } catch ( PDOException  $ex ) {
+     echo  $ex -> getMessage ();
+     }
+   }
+ }
 ?>
