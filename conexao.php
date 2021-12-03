7e6ed3502fa76734527d7af4c5e7808180b91ce3
marcelafrutuoso <88401869+marcelafrutuoso@users.noreply.github.com>
2021-12-03 08:48:46 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,27 @@
+<?php
+
+class conexao{
+  private $servidor;
+  private $banco;
+  private $usuario;
+  private $senha;
+  private  static  $pdo ;
+  
+  public function __construct () {
+    $this ->servidor = 'localhost';
+    $this ->banco = 'av2';
+    $this ->usuario = 'root';
+    $this ->senha = '';
+  }
+  public function conectar() {
+    try {
+      if ( is_null ( self :: $pdo )) {
+        self :: $pdo = new  PDO( $this->servidor , $this->banco , $this->usuario , $this->senha );
+      }
+      return self :: $pdo;
+    } catch ( PDOException $ex ) {
+      echo $ex ->getMessage();
+      }
+    }
+  }
+?>
