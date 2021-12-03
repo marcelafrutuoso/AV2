546c01abbe1c32a086d8bc3397248e14ef0925a0
marcelafrutuoso <88401869+marcelafrutuoso@users.noreply.github.com>
2021-12-03 09:09:59 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -16,7 +16,7 @@ class conexao{
   public function conectar() {
     try {
       if ( is_null ( self :: $pdo )) {
-        self :: $pdo = new  PDO( $this->servidor , $this->banco , $this->usuario , $this->senha );
+        self :: $pdo = new  PDO( "mysql: host =" . $this->servidor . "; dbname =" . $this->banco , $this->usuario , $this->senha );
       }
       return self :: $pdo;
     } catch ( PDOException $ex ) {
